e1aae43f7bc87051412da26e24415cfd77a948bb
Artem Gorbachev <gorbachev99artem@gmail.com>
2019-02-07 23:09:22 +0300

    Task6

---
+++ Task6/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    enum SortBy
+    {
+        PersonalInformation,
+        SalaryValue
+    }
+
+    
+    class Employee : IComparable<Employee>
+    {
+        //personal information
+        public string Name { get; private set; }
+        public int? PhoneNumber { get; set; }
+        public string PositionHeld { get; set; }
+       
+        //salary value
+        public int SalaryValue { get; set; }
+
+        //ctor
+        public Employee(string name, string positionHold, int salaryValue, int? phoneNumber = null)
+        {
+            Name = name;
+            PositionHeld = positionHold;
+            SalaryValue = salaryValue;
+            PhoneNumber = phoneNumber;
+        }
+
+        //get information about employee
+        public void GetInformation()
+        {
+            //The method PadRight(\d*) is used for everything to be prettily write.
+            Console.Write($"Name: {Name.PadRight(8)}\tPasition hold: {PositionHeld.PadRight(15)}\tSalary value: {SalaryValue:C2}");
+            if (PhoneNumber != null)
+                Console.WriteLine($"\tPhone number {PhoneNumber:+375(##)###-##-##}");
+            else
+                Console.WriteLine();
+        }
+
+        //sorting by parameter
+        public static int CompareBySalaryValue(Employee x, Employee y)
+        {
+            return x.SalaryValue.CompareTo(y.SalaryValue);
+        }
+        public static int CompareByPersonalInformation(Employee x, Employee y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+
+        //default sorting
+        public int CompareTo(Employee other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[] { new Employee("Artem", "merchandiser", 900), new Employee("Gleb", "seller", 500), new Employee("Kirill", "directors", 2000, 295389238),
+                                                    new Employee("Bob", "loader", 650), new Employee("Pasha", "security", 550), new Employee("Masha", "main seller", 700)};
+            foreach (var item in employees)
+                item.GetInformation();
+
+            Console.WriteLine();
+            //compare by person information parameter(Name)
+            Array.Sort(employees, Employee.CompareByPersonalInformation);
+
+            foreach (var item in employees)
+                item.GetInformation();
+
+            Console.WriteLine();
+            //compare by salary value parameter(SalaryValue)
+            Array.Sort(employees, Employee.CompareBySalaryValue);
+
+            foreach (var item in employees)
+                item.GetInformation();
+
+            Console.WriteLine();
+            //default compare (by Name)
+            Array.Sort(employees);
+
+            foreach (var item in employees)
+                item.GetInformation();
+
+            Console.ReadKey();
+        }
+    }
+}
